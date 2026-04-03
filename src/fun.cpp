5c34c0a41b56850779cef228776830df35ab984e
mvlazarenko <mvlazarenko@phoenixit.ru>
2026-04-03 19:14:36 +0300

    b9 Лазаренко М.В.

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,110 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    int state = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = str[i];
+        int space = (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+        int digit = (c >= '0' && c <= '9');
+        switch (state) {
+            case 0:
+                if (!space) {
+                    if (digit) state = 2;
+                    else
+                        state = 1;
+                }
+                break;
+            case 1:
+                if (space) {
+                    count++;
+                    state = 0;
+                } else if (digit) {
+                    state = 2;
+                }
+                break;
+            case 2:
+                if (space) state = 0;
+                break;
+        }
+    }
+    if (state == 1) count++;
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    unsigned int count = 0;
+    int state = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = str[i];
+        int space = (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+        int upper = (c >= 'A' && c <= 'Z');
+        int lower = (c >= 'a' && c <= 'z');
+        switch (state) {
+            case 0:
+                if (!space) {
+                    if (upper) state = 1;
+                    else
+                        state = 3;
+                }
+                break;
+            case 1:
+                if (space) {
+                    count++;
+                    state = 0;
+                } else if (lower) {
+                    state = 2;
+                } else {
+                    state = 3;
+                }
+                break;
+            case 2:
+                if (space) {
+                    count++;
+                    state = 0;
+                } else if (!lower) {
+                    state = 3;
+                }
+                break;
+            case 3:
+                if (space) state = 0;
+                break;
+        }
+    }
+    if (state == 1 || state == 2) count++;
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    unsigned int words = 0;
+    unsigned int total = 0;
+    unsigned int len = 0;
+    int state = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        char c = str[i];
+        int space = (c == ' ' || c == '\t' || c == '\n' || c == '\r');
+        switch (state) {
+            case 0:
+                if (!space) {
+                    len = 1;
+                    state = 1;
+                }
+                break;
+            case 1:
+                if (space) {
+                    words++;
+                    total += len;
+                    state = 0;
+                } else {
+                    len++;
+                }
+                break;
+        }
+    }
+    if (state == 1) {
+        words++;
+        total += len;
+    }
+    if (words == 0) return 0;
+    return (total + words / 2) / words;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 // Copyright 2022 UNN-IASR
+#include <iostream>
 #include "fun.h"
 
 int main() {
+    const char *s1 = "sun rain w1nd cloud st0rm sky";
+    const char *s2 = "Moscow London paris New york Berlin";
+    const char *s3 = "war and peace";
+
+    std::cout << "faStr1: слова без цифр в \"" << s1 << "\": "
+              << faStr1(s1) << std::endl;
+    std::cout << "faStr2: слова с заглавной в \"" << s2 << "\": "
+              << faStr2(s2) << std::endl;
+    std::cout << "faStr3: средняя длина в \"" << s3 << "\": "
+              << faStr3(s3) << std::endl;
     return 0;
 }
